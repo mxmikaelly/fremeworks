ca7d6a21319a28b48f74e9324921a40f00246370
VitorSLima <dev.vitorlima@gmail.com>
2026-04-10 12:02:25 -0400

    Update package-lock.json to add 'peer' property for multiple dependencies; change Prisma client provider to 'prisma-client-js' in schema.prisma; refactor imports in prisma.database.ts for consistency.

--- src/shared/databases/prisma.database.ts
+++ src/shared/databases/prisma.database.ts
@@ -1,15 +1,15 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client/extension";
-
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  todo: any;
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL as string,
     });
+    // Prisma types are generated at runtime; eslint cannot fully resolve this call.
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     super({ adapter });
   }
 }
